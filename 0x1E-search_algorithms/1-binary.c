ada6501c5b934f9695e34484d087326a8c67c7d8
favour2333 <kingsleyfavour999@gmail.com>
2023-04-23 16:43:47 +0000

    initial

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,29 +1,51 @@
-#include <stdio.h>
+#include "search_algos.h"
 
-int binary_search(int arr[], int left, int right, int x) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == x) {
-            return mid;
-        } else if (arr[mid] < x) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return -1;
+/**
+ * print_arr - helper func to print array everytime array is halved
+ * @array: array
+ * @l: left index of original array
+ * @r: right index of original array
+ */
+void print_arr(int *array, size_t l, size_t r)
+{
+	size_t i = 0;
+
+	printf("Searching in array: ");
+	for (i = l; i <= r; i++)
+	{
+		if (i != r)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
 }
 
-int main() {
-    int arr[] = { 2, 4, 6, 8, 10 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 8;
-    int result = binary_search(arr, 0, n - 1, x);
-    if (result == -1) {
-        printf("Element not found.\n");
-    } else {
-        printf("Element found at index %d.\n", result);
-    }
-    return 0;
-}
+/**
+ * binary_search - search mids
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid = 0;
+	size_t l = 0;
+	size_t r = size - 1;
 
+	if (!array)
+		return (-1);
+
+	while (l <= r)
+	{
+		print_arr(array, l, r);
+		mid = (l + r) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			r = mid - 1;
+		else
+			l = mid + 1;
+	}
+	return (-1);
+}
